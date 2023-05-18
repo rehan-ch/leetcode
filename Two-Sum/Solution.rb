5f85fd1de18c8a664f588b6a50b354743b5129a7
Muhammad Rehan <rehanch642@gmail.com>
2023-05-18 12:52:01 +0500

    sum of two numbers

--- Two-Sum/Solution.rb
+++ Two-Sum/Solution.rb
@@ -1,3 +1,14 @@
 def two_sum(nums, target)
- 
+    complement_map = {}
+    nums.each_with_index do |num, i|
+        complement = target - num
+        if complement_map.key?(complement)
+            print [complement_map[complement], i]
+            break
+        end
+        complement_map[num] = i
+    end
+    []
 end
+
+two_sum([3,2,4], 6)
