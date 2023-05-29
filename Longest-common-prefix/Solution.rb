3dcdd674be92f8a38c21779bcfbd45b1cbb9d2a8
Muhammad Rehan <rehanch642@gmail.com>
2023-05-29 21:49:04 +0500

    feat: longest common prefix solve

--- Longest-common-prefix/Solution.rb
+++ Longest-common-prefix/Solution.rb
@@ -1,4 +1,16 @@
 def longest_common_prefix(strs)
     return "" if strs.empty?
+  
+    # Find the string with the minimum length in the array
+    min_length = strs.min_by(&:length).length
 
+    # Iterate through the characters of the minimum length string
+    min_length.times do |i|
+      char = strs[0][i]
+      # Check if the character is common to all strings
+      return strs[0][0...i] unless strs.all? { |str| str[i] == char }
+    end
+  
+    # If all characters match, return the minimum length string
+    strs[0][0...min_length]
   end
